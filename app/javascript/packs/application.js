3decc137b9ce6828eb7ced0b86b040a4777faf6e
Artem Rakov <scoff1k@gmail.com>
2018-12-01 15:42:35 +0300

    added vue to the project

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,21 @@
 import "bootstrap";
+import Vue from 'vue/dist/vue.esm'
+
+
+import Dashboard from '../dashboard.vue'
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const element = document.querySelector('#dashboard')
+  if (element != undefined) {
+      const app = new Vue({
+          el: element,
+          data: {
+
+          },
+          template: "<Dashboard  />",
+          components: { Dashboard }
+      })
+  }
+})
