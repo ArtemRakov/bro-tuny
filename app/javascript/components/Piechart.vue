7fcd8a8e9621ae3bae64c8d6c7173601f0822451
Artem Rakov <scoff1k@gmail.com>
2018-12-02 14:28:30 +0300

    done

---
+++ app/javascript/components/Piechart.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+     <vue-easy-pie-chart
+        :percent="70"
+        :rotate="0"
+        :scale-color="0"
+        :size="200"
+        :line-width="10"
+        class="dashboard__piechart"
+        font-size="38px"
+        line-cap="round"
+        bar-color="#F4580F"
+
+     ></vue-easy-pie-chart>
+  </div>
+</template>
+
+<script>
+  import VueEasyPieChart from 'vue-easy-pie-chart'
+  import 'vue-easy-pie-chart/dist/vue-easy-pie-chart.css'
+export default {
+    props: ['progress'],
+    components:{ VueEasyPieChart }
+}
+</script>
--- app/javascript/dashboard.vue
+++ app/javascript/dashboard.vue
@@ -1,49 +1,72 @@
 <template>
   <div class="wrapper_dashboard">
-    <div class="container">
-      <div>
-        <ul class="progressbar">
-          <li  :class="{ active: stage.status === 'done' }" :key="stage.id" v-for="stage in sortedStages"> 
-            <div @mouseover="changeTab(stage)"  :class="{ progressbar_dot_active: stage.status === 'done', progressbar_dot_orange: stage.status === 'active' }" class="progressbar_dot">  </div>
-            <div class="progressbar_active_tab"> {{ text_truncate(stage.title, 30) }}  </div>
+    <div class="dashboard__container">
+    <h1 class="header_dashboard text-center"> Ход строительства </h1>
+      <div class="dashboard__cards">
+        <div class="dashboard__stages">
+            <ul class="progressbar">
+              <li  :class="{ active: stage.status === 'done' }" :key="stage.id" v-for="stage in sortedStages"> 
+                <div @mouseover="changeTab(stage)"  :class="{ progressbar_dot_active: stage.status === 'done', progressbar_dot_orange: stage.status === 'active' }" class="progressbar_dot">  </div>
+                <div class="progressbar_active_tab"> {{ text_truncate(stage.title, 30) }}  </div>
+                <transition name="fade" mode="out-in">
+                  <div v-if="stage.id === activeTab.id" class="progressbar_triangle" >  </div>
+                </transition>
+              </li>
+            </ul>
+
+
             <transition name="fade" mode="out-in">
-              <div v-if="stage.id === activeTab.id" class="progressbar_triangle" >  </div>
-            </transition>
-          </li>
-        </ul>
+              <div class="dashboard__info" :key="activeTab.id">
+                <div class="dashboard__main">
+                  <h2 class="dashboard__header"> {{ activeTab.title }} </h2>
+                  <p class="dashboard__description" v-html="activeTab.description"> </p>
+                </div>
+                <div class="dashboard__secondary">
+                  <div v-if="activeTab.photo" class="dashboard__secondary-photo" :style="{'background-image': 'url(http://172.21.47.224:8081/api/image/' + activeTab.photo  + ')'}">
+                  </div>
+                  <!-- <div class="dashboard__secondary-photo-2" :style="{'background-image': 'url(' + updateImage()  +  '+ )'}" >
+                  </div> -->
+                </div>
+              </div>   
+            </transition> 
+        </div>
+        <div class="dashboard__house">
+          <!-- <div class="dashboard__secondary-photo-2" :style="{'background-image': 'url(http://178.210.43.234:874/avreg-cgi/jpg/image.cgi?camera=2&ab=dmlld2VyOjE1OTc1Mw==&_=1543737832368)'}" > -->
+          <!-- </div> -->
+          <div class="dashboard__house-top">
+            <h3 class="dashboard__house-header"> Информация о доме </h3>
+            <p class="dashboard__house-info"> Название: Чайка </p>
+            <p class="dashboard__house-info"> Адрес: Чайковского, 10 </p>
+            <p class="dashboard__house-info"> Дата окончания: 20.11.2020 </p>
+          </div>
+
+          <!-- <div class="dashboard__img"> -->
+              <!-- <div class="dashboard__secondary-photo-2" :style="{'background-image': 'url(' + updateImage()  +  '+ )'}" >
+              </div>  -->
+              <img class="dashboard__picture" src="http://178.210.43.234:874/avreg-cgi/jpg/image.cgi?camera=2&ab=dmlld2VyOjE1OTc1Mw==&_=1543737832368" alt="">
+            <!-- <piechart @progress="100"> </piechart> -->
+          <!-- </div> -->
+        </div>
       </div>
-
-
-        <transition name="fade" mode="out-in">
-          <div class="dashboard__info" :key="activeTab.id">
-            <div class="dashboard__main">
-              <h2 class="dashboard__header"> {{ activeTab.title }} </h2>
-              <p class="dashboard__description"> {{ activeTab.description }} </p>
-            </div>
-            <div class="dashboard__secondary">
-              <div v-if="activeTab.photo" class="dashboard__secondary-photo" :style="{'background-image': 'url(http://172.21.47.224:8081/api/image/' + activeTab.photo  + ')'}">
-              </div>
-              <div class="dashboard__secondary-photo-2" :style="{'background-image': 'url(' + updateImage()  +  '+ )'}" >
-              </div>
-            </div>
-          </div>   
-        </transition> 
-    </div>
+  </div>
   </div>
 </template>
 
 
 <script>
+
+import piechart from './components/Piechart.vue';
+
 export default {
   props: ['stages', 'house', 'houseComplex'],
   data() {
     return {
-      activeTab: this.stages.find(element => { return element.status === 'active' }),
+      activeTab: {},
     }
   },
   methods: {
     changeTab(stage) {
-      if (stage.id === this.activeTab.id || stage.status === 'not_started')  {
+      if (stage.id === this.activeTab.id)  {
 
       }
       else {
@@ -68,9 +91,10 @@ export default {
     }
   },
   created() {
-    setTimeout(() => {
-      this.updateImage()
-    }, 1000);
+    const tab = this.stages.find(element => { return element.status === 'active' })
+    if (tab == undefined) {
+      this.activeTab = this.stages.first
+    }
   },
   computed: {
     sortedStages: function() {
@@ -83,6 +107,9 @@ export default {
       }
       return this.stages.sort(compare);
     }
+  },
+  components: {
+    piechart: piechart
   }
 }
 </script>
